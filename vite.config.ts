cb3ff9200485d422de6b0ff7b1c3f969485557f5
nafweb1 <webmaster.nafweb@gmail.com>
2024-11-03 17:36:26 +0100

    Updated package.json

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,11 @@
-import { sveltekit } from '@sveltejs/kit/vite';
+import { svelte } from '@sveltejs/vite-plugin-svelte';
 import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
-	plugins: [sveltekit()],
-	test: {
-		include: ['src/**/*.{test,spec}.{js,ts}']
+	plugins: [svelte()],
+	css: {
+		postcss: {
+			plugins: [require('tailwindcss'), require('autoprefixer')]
+		}
 	}
 });
